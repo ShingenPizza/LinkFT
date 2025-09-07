a510b1178ce0c39bdfc4e9cf464a584f2081bb95
Shingen <piotr.kunicki@o2.pl>
2025-09-07 20:06:13 +0200

    Polished the moduleupdate script.

--- SteamLinkVRCFTModule/res/moduleupdate.py
+++ SteamLinkVRCFTModule/res/moduleupdate.py
@@ -1,15 +1,18 @@
 import json
 import datetime
 from json import JSONEncoder
-f = open("module.json")
 
-data = json.load(f)
-versionstr = data["Version"]
-x,y,z = versionstr.split(".")
-z = str(int(z)+1)
-data["Version"] = x+ "." + y + "." + z
-data["LastUpdated"] = datetime.datetime.now().isoformat()
-f.close()
-f = open("module.json", "w+")
-json.dump(data, f, ensure_ascii=False, indent=4)
-f.close()
+
+def main():
+    with open('module.json', 'r') as f:
+        data = json.load(f)
+    versionstr = data['Version']
+    x, y, z = versionstr.split('.')
+    data['Version'] = f'{x}.{y}.{int(z) + 1}'
+    data['LastUpdated'] = datetime.datetime.now().isoformat()
+    with open('module.json', 'w', newline='\n') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    main()
